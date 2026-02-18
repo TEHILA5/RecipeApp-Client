1cf0354c55f4a18a49d67b948cbd801d420f4c00
Tehila Alon <tehila54345@gmail.com>
2026-02-18 20:39:48 +0200

    Configure store.ts

---
+++ src/app/store.ts
@@ -0,0 +1,27 @@
+// ===============================================
+// Redux Store - src/app/store.ts
+// ===============================================
+
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/auth/redux/authSlice.ts';
+import recipeReducer from '../features/recipe/redux/recipeSlice.ts';
+import uiReducer from '../redux/slices/uiSlice.ts';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    recipes: recipeReducer,
+    ui: uiReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // התעלם מ-functions ב-Redux (למשל ב-modal)
+        ignoredActions: ['ui/openModal'],
+        ignoredPaths: ['ui.modal.onConfirm'],
+      },
+    }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
