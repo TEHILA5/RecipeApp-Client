27a7e5f0bd6e383bd8ec5bac629633cc2042dbc0
Tehila Alon <tehila54345@gmail.com>
2026-02-18 20:42:28 +0200

    Add User pages (MyRecipesPage, ProfilePage)

---
+++ src/features/user/pages/MyRecipesPage.tsx
@@ -0,0 +1,8 @@
+export default function MyRecipesPage() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>My Recipes 💾</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
